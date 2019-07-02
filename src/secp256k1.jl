922142c4046150ce862e83e3ca555e016ad554de
roshii <roshii@riseup.net>
2019-07-02 14:55:44 +0000

    Parametric Signature

--- src/secp256k1.jl
+++ src/secp256k1.jl
@@ -4,12 +4,13 @@ module secp256k1
 
 using BitConverter
 import Base: +, -, *, ^, /, ==, inv, sqrt, show, div
-export ∞, KeyPair, ECDSA
+export ∞, Signature, KeyPair, ECDSA
 
 include("lib/errors.jl")
 include("lib/FieldElement.jl")
 include("lib/Infinity.jl")
 include("lib/Point.jl")
+include("lib/scheme-types.jl")
 include("lib/ECDSA.jl")
 
 end # module
